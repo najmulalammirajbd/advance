ce4fb9d4486ed687af2fe5a7fcbedcd00c7c2112
najmulalammirajbd <najmulalammiraj@gmail.com>
2020-08-07 12:23:57 +0600

    my work done

---
+++ null-undefinde.js
@@ -0,0 +1,18 @@
+let phaki;
+function add(num1,num2){
+    console.log(num1+num2);
+    return
+}
+
+function add(num1,num2){
+    console.log(num1,num2);
+}
+
+const student = {name:"najmul alam mirja" , phone:013031111111111};
+console.log(student.miraj);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,5,7];
+console.log(ages[11]);
